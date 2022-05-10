59dfa2c1ac911379d365ab5cb2db6237ef894e4d
Baboucarr Foon <baboucarrfoon@gmail.com>
2022-05-10 16:36:14 +0000

    motopool update

--- motopool/users/views.py
+++ motopool/users/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def login(request):
+   # return redirect('dashboard')
    return render(request, "users/login.html")
+
+def dashboard(request):
+    return render(request, "users/dashboard.html")
